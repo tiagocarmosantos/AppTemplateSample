cbe777765e0c1dd53b544f8bb831a40bcddb8313
Tiago Carmo Santos <tiagocarmosantos@hotmail.com>
2020-07-18 10:45:50 -0300

    Automatic Commit

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,19 @@ require('./gulpTasks/deps')
 require('./gulpTasks/server')
 
 gulp.task('default', () => {
+	if (util.env.build) {
+		sequence('deps', 'app')
+	}
+
 	if (util.env.dev) {
 		sequence('deps', 'app', 'devServer')
 	}
 
-	if (util.env.production) {
-		sequence('deps', 'app')
+	if (util.env.quality) {
+		sequence('deps', 'app', 'qaServer')
 	}
 
+	if (util.env.production) {
+		sequence('deps', 'app','prodServer')
+	}
 })
